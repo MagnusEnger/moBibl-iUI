a3824620a0894439b6251d7a1af2b66f53a2bdae
Magnus Enger <magnus@enger.(none)>
2010-07-23 12:23:05 +0000

    Implement real searching. Issue 4.

--- iphone.js
+++ iphone.js
@@ -44,9 +44,10 @@ $(document).ready(function(){
 function getSearchResults() {
 
   var q = $('#q').val();
+  var library = $.getUrlVar('lib');
   // Use dummy: 'true' as argument to glitre/api/ to get back dummy data
-  $.get('glitre/api/', { q: q, format: 'plugin.mobibl' }, function(html) { $('#searchresults').empty(); $('#searchresults').append(html); });
-  $.get('glitre/api/', { q: q, format: 'plugin.mobiblfull' }, function(html) { $('body').append(html); });
+  $.get('/mobibl/glitre/api/', { q: q, library: library, format: 'plugin.mobibl' }, function(html) { $('#searchresults').empty(); $('#searchresults').append(html); });
+  $.get('/mobibl/glitre/api/', { q: q, library: library, format: 'plugin.mobiblfull' }, function(html) { $('body').append(html); });
   return false;
 
 }
@@ -60,3 +61,24 @@ function saveSettings() {
 function loadSettings() {
   $('#theme').val(localStorage.theme);
 }
+
+// Thanks to: http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html
+$.extend({
+  getUrlVars: function(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      // The following two lines remove anything after a #
+      var subhash = hash[1].split('#');
+      hash[1] = subhash[0];
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name){
+    return $.getUrlVars()[name];
+  }
+});
